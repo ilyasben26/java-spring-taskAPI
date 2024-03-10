5c64420cd046e62863c2e11a62e23346b8f6d22e
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-03-10 08:46:27 +0100

    added Role class as ManyToMany with User

--- src/main/java/com/ilyasben/taskAPI/model/User.java
+++ src/main/java/com/ilyasben/taskAPI/model/User.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -28,10 +29,15 @@ public class User {
     private String username;
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+        inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+    private List<Role> roles = new ArrayList<>();
 
     @CreationTimestamp
     private LocalDateTime createdOn;
     @UpdateTimestamp
     private LocalDateTime updatedOn;
+
 
 }
