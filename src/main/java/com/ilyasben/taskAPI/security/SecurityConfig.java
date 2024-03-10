f2fdb5d8efd87d35dfe02be4cb2bf38e7b745469
IlyasBen <73348981+ilyasben26@users.noreply.github.com>
2024-03-10 08:30:04 +0100

    added initial Spring Security configuration

---
+++ src/main/java/com/ilyasben/taskAPI/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.ilyasben.taskAPI.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .csrf().disable() // TODO: turn CSRF back on in prod
+                .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+
+        return httpSecurity.build();
+    }
+
+    // TODO: figure out how to use my existing user class with the spring user class
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.builder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+}
